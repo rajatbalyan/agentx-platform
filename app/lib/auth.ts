792bd8c4f32cb019a03ae31c30924e3fff47d4ed
Rajat Balyan <balyanrajat1812@gmail.com>
2025-03-22 00:46:15 +0530

    update 0.1

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,5 +1,24 @@
 import { getServerSession } from "next-auth";
 import { authConfig } from "./auth.config";
+import { AuthOptions } from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+
+if (!process.env.GITHUB_ID || !process.env.GITHUB_SECRET) {
+  throw new Error('Missing GitHub OAuth credentials');
+}
+
+export const authOptions: AuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: '/auth/signin',
+  },
+};
 
 export async function getSession() {
   return await getServerSession(authConfig);
